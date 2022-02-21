d8f7fe57d24e63faabec6d7021b4c9691a5668b3
Alec Edgington <alec.edgington@cambridgequantum.com>
2022-02-21 16:32:15 +0000

    Copy all lib files.

--- recipes/tket-tests/test_package/conanfile.py
+++ recipes/tket-tests/test_package/conanfile.py
@@ -45,11 +45,10 @@ class TketTestsTestConan(ConanFile):
         if not tools.cross_building(self):
             lib_files = os.listdir(os.path.join(self.install_folder, "lib"))
             for lib_file in lib_files:
-                if "tket" in lib_file:
-                    copyfile(
-                        os.path.join(self.install_folder, "lib", lib_file),
-                        os.path.join("bin", lib_file),
-                    )
+                copyfile(
+                    os.path.join(self.install_folder, "lib", lib_file),
+                    os.path.join("bin", lib_file),
+                )
             cmd_extra = "" if platform.system() == "Linux" else ' "~[latex]"'
             os.chdir("bin")
             self.run(os.path.join(os.curdir, "test_tket" + cmd_extra))
